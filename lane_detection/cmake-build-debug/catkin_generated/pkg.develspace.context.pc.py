7f971a4fc85db51159bc95e86856dcc9613a9ddd
moon <myr9691@naver.com>
2020-05-20 16:13:41 +0900

    delete

--- lane_detection/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/moon/catkin_ws/src/lane_detection/include".split(';') if "/home/moon/catkin_ws/src/lane_detection/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "cv_bridge;image_transport;roscpp;sensor_msgs;std_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-llane_detection".split(';') if "-llane_detection" != "" else []
-PROJECT_NAME = "lane_detection"
-PROJECT_SPACE_DIR = "/home/moon/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
--- lane_detection/cmake-build-debug/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/moon/catkin_ws/src/lane_detection/cmake-build-debug/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
